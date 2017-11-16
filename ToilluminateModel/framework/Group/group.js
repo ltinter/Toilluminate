7d58c6491504ab33136d8fadabb711c0a0c6cf9b
Ta-LT <LT_Inter@Hotmail.com>
2017-11-17 02:18:53 +0800

    Fix

--- ToilluminateModel/framework/Group/group.js
+++ ToilluminateModel/framework/Group/group.js
@@ -368,8 +368,8 @@
                 success: function (data) {
                     var div_forcedplaylists = $('#forcedplaylists');
                     var forcedplaylists = div_forcedplaylists.find(".m-portlet.m-portlet--warning.m-portlet--head-sm");
+                    playListgroup =[];
                     if (forcedplaylists.length > 0) {
-                        playListgroup = [];
                         $.each(forcedplaylists, function (index, forcedplaylist) {
                             var playlistItem = {};
                             var forcedplaylistID = $(forcedplaylist).attr('playlistId');
@@ -1142,7 +1142,7 @@
     $("#edit_player").click(function () {
         var selected = datatable.setSelectedRecords().getSelectedRecords();
         selectPlayerdata = selected;
-        if (selectPlayerdata || selectPlayerdata.length == 0) { return; }
+        if (selectPlayerdata.length == 0) { return; }
         var allPlayerNames = "";
         $.each(selectPlayerdata, function (playerIndex, playerItem) {
             allPlayerNames += ", " + $(playerItem).data().obj.PlayerName;
