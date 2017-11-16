3d7d3141ad3a6272834e4ffb1a3ba3718ec14760
YX66 <yx@USER-20170606VA>
2017-11-17 01:10:20 +0800

    55

--- ToilluminateModel/framework/Group/group.js
+++ ToilluminateModel/framework/Group/group.js
@@ -1142,7 +1142,7 @@
     $("#edit_player").click(function () {
         var selected = datatable.setSelectedRecords().getSelectedRecords();
         selectPlayerdata = selected;
-        if (selectPlayerdata.length == 0) { return; }
+        if (selectPlayerdata || selectPlayerdata.length == 0) { return; }
         var allPlayerNames = "";
         $.each(selectPlayerdata, function (playerIndex, playerItem) {
             allPlayerNames += ", " + $(playerItem).data().obj.PlayerName;
