d1f4a2fca44f91a9cdff07db7309e3cb2336f78f
YX66 <yx@USER-20170606VA>
2017-11-16 23:00:04 +0800

    fix

--- ToilluminateModel/framework/PlaylistEditor/playlistEditor.js
+++ ToilluminateModel/framework/PlaylistEditor/playlistEditor.js
@@ -443,7 +443,7 @@
                         divselectFile.empty();
                     }
                     var screenshot = new Image();
-                    screenshot.src = $(item).data().obj.FilelUrl;
+                    screenshot.src = $(item).data().obj.FileUrl;
                     screenshot.id = $(item).data().obj.FileID;
                     $(screenshot).css({ "max-height": "150px", "max-width": "200px","padding":"5px" });
                     divselectFile.append(screenshot);
