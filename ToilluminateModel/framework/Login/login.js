7589cd06dadb4f183976ce03e447670201352d0b
YX66 <yx@USER-20170606VA>
2017-11-30 14:16:24 +0800

    fix

--- ToilluminateModel/framework/Login/login.js
+++ ToilluminateModel/framework/Login/login.js
@@ -28,7 +28,7 @@
 
 
     };
-
+    var login = $('#m_login');
     $("#m_login_signin_submit").click(function (e) {
         
         e.preventDefault();
@@ -66,7 +66,87 @@
         });
     });
 
+    $('#m_login_signup').click(function (e) {
+        e.preventDefault();
+        displaySignUpForm();
+    });
+    var displaySignUpForm = function () {
+        login.removeClass('m-login--forget-password');
+        login.removeClass('m-login--signin');
 
+        login.addClass('m-login--signup');
+        login.find('.m-login__signup').animateClass('flipInX animated');
+    }
+    //var handleSignUpFormSubmit = function () {
+        $('#m_login_signup_submit').click(function (e) {
+            e.preventDefault();
+
+            var btn = $(this);
+            var form = $(this).closest('form');
+
+            form.validate({
+                rules: {
+                    username: {
+                        required: true
+                    },
+                    password: {
+                        required: true
+                    },
+                    rpassword: {
+                        required: true
+                    },
+                    agree: {
+                        required: true
+                    }
+                }
+            });
+
+            if (!form.valid()) {
+                return;
+            }
+
+            btn.addClass('m-loader m-loader--right m-loader--light').attr('disabled', true);
+
+
+            $.insmFramework('creatUser', {
+                userName: 'test',
+                groupID: 1,
+                password: 'admin',
+                emailAddress: '',
+                comments: '',
+                settings: '',
+                success: function (response, status, xhr, $form) {
+                    // similate 2s delay
+                    setTimeout(function () {
+                        btn.removeClass('m-loader m-loader--right m-loader--light').attr('disabled', false);
+                        form.clearForm();
+                        form.validate().resetForm();
+
+                        // display signup form
+                        displaySignInForm();
+                        var signInForm = login.find('.m-login__signin form');
+                        signInForm.clearForm();
+                        signInForm.validate().resetForm();
+
+                        showErrorMsg(signInForm, 'success', 'Thank you. To complete your registration please check your email.');
+                    }, 2000);
+                }
+            })
+
+            //form.ajaxSubmit({
+            //    url: '',
+                
+            //});
+        });
+    //}
+
+    var displaySignInForm = function () {
+        login.removeClass('m-login--forget-password');
+        login.removeClass('m-login--signup');
+
+        login.addClass('m-login--signin');
+        login.find('.m-login__signin').animateClass('flipInX animated');
+    }
     $.login = function (method) {
         if (methods[method]) {
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
